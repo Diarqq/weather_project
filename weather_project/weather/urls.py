acf0e82c674b2256987c6bb6fbcf230b76d110b4
Diarqq <kiviivi58@gmail.com>
2025-11-17 17:50:28 +0300

    Worked with urls

--- weather_project/weather/urls.py
+++ weather_project/weather/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'weather'
 urlpatterns = [
-    path('',views.weather_query, name = 'query')
-    path('api/', views.weather_api,name='api')
-    path('history/',views.query_history,name='history')
+    path('',views.weather_query, name = 'query'),
+    path('api/', views.weather_api,name='api'),
+    path('history/',views.query_history,name='history'),
 ]
