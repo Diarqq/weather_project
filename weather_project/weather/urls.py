3e98118e52a0e0526f59c9e47d257c881298b8bf
Diarqq <kiviivi58@gmail.com>
2025-11-17 17:44:40 +0300

    Worked with urls

---
+++ weather_project/weather/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'weather'
+urlpatterns = [
+    path('',views.weather_query, name = 'query')
+    path('api/', views.weather_api,name='api')
+    path('history/',views.query_history,name='history')
+]
