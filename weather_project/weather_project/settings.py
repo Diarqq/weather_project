0e89dd5718349178dc43fdffc3159d21cb628153
Diarqq <kiviivi58@gmail.com>
2025-11-20 00:53:12 +0300

    Fixed little bug

--- weather_project/weather_project/settings.py
+++ weather_project/weather_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import sys
 from pathlib import Path
 import environ
 
@@ -136,6 +136,13 @@ CACHES = {
         },
     }
 }
+if 'test' in sys.argv:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        }
+    }
+
 
 LOGGING = {
     "version": 1,
