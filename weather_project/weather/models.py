67601276f9f2938e034c77ae9e37c6c224e1075e
Diarqq <kiviivi58@gmail.com>
2025-11-19 16:17:34 +0300

    Updated model

--- weather_project/weather/models.py
+++ weather_project/weather/models.py
@@ -10,6 +10,8 @@ class WeatherQuery(models.Model):
     weather_description = models.CharField(max_length=250)
     units = models.CharField(max_length=10) #если не в системе СИ, а имперской, например, для Америки
     served_from_cache = models.BooleanField(default=False)
+    ip_address = models.GenericIPAddressField(null=True, blank=True)  # для rate limiting
+    raw_data = models.JSONField(default=dict, blank=True)  # полные данные от API
 
     def __str__(self):
         return f'{self.city_name} - {self.temperature}'
